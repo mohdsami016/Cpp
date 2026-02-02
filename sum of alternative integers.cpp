338fe5caf3a8dc3714bf3540d9c7b2c13b310ee0
mohdsami016 <mohdsamiit016@gmail.com>
2026-02-02 15:04:26 +0530

    Add files via upload

---
+++ sum of alternative integers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int sumarray(int a[],int n){
+	int sum=0;
+	for(int i=0;i<n;i=i+2){
+		sum=a[i]+sum;
+	}
+	return sum;
+}
+int main(){
+	int a[]={65,87,2,4,56};
+	int n=sizeof(a)/sizeof(a[0]);
+	cout<<"sum of alternative integers in array elements ="<<sumarray(a,n);
+	return 0;
+}
